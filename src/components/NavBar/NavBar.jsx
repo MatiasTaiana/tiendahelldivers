43d3edea44fc7f7a58d70516335e12c1890c675b
Matias Taiana <matiastaiana@hotmail.com>
2024-11-26 18:51:45 -0300

    imagen navbar 3

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ import CartWidget from '../CartWidget/CartWidget';
 const NavBar = () => {
   return (
     <nav>
-      <Link to="/"><img src="../assets/super-earth-helldivers-svg-logo-v0-0cvbn5nesrvc1.webp" alt="Helldivers Logo" /></Link>
+      <Link to="/"><img src="https://preview.redd.it/super-earth-helldivers-svg-logo-v0-0cvbn5nesrvc1.png?width=1024&format=png&auto=webp&s=e3fa0b71beb271bf9164688c4bb23703f7eca352" alt="Helldivers Logo" /></Link>
       <Link to="/terminids"><button className='buttonNav'>Terminids</button></Link>
       <Link to="/automatons"><button className='buttonNav'>Automatons</button></Link>
       <Link to="/weapons"><button className='buttonNav'>Weapons</button></Link>
