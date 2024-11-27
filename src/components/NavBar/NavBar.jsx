2ffe05224318e0f83b756b57c4940f75ef695ec0
Matias Taiana <matiastaiana@hotmail.com>
2024-11-26 21:45:21 -0300

    estilos navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import CartWidget from '../CartWidget/CartWidget';
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav >
       <Link to="/"><img src="https://preview.redd.it/super-earth-helldivers-svg-logo-v0-0cvbn5nesrvc1.png?width=1024&format=png&auto=webp&s=e3fa0b71beb271bf9164688c4bb23703f7eca352" alt="Helldivers Logo" /></Link>
       <Link to="/terminids"><button className='buttonNav'>Terminids</button></Link>
       <Link to="/automatons"><button className='buttonNav'>Automatons</button></Link>
